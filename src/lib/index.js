49f90a954655c254978fd8182ca6a41053c7d572
danielablancom <blancodanielac@gmail.com>
2020-08-04 10:58:47 -0400

    Estructura Css

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,50 @@ export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
+/* eslint-disable no-alert */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+// aqui exportaras las funciones que necesites
+export const myFunction = () => {
+};
+// SingUp
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // para cancelar el evento del reinicio del formulario
+  const signupUserName = document.querySelector('#singup-username').value;
+  const signupEmail = document.querySelector('#signup-email').value;
+  const signupPassword = document.querySelector('#signup-password').value;
+  const signupPassword2 = document.querySelector('#signup-password2').value;
+  auth
+    .createUserWithEmailAndPassword(signupEmail, signupPassword)
+    .then((userCredential) => {
+    });
+});
+// Signin
+const signInForm = document.querySelector('#login-form');
+signInForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Para cancelar el evento del reinicio del formulario
+  const email = document.querySelector('#login-email').value;
+  const password = document.querySelector('#login-password').value;
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Limpiar el form
+      signupForm.reset();
+      console.log('shao');
+    });
+  firebase.auth().signInWithEmailAndPassword(email, password).catch((error) => {
+  // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    if (errorCode == 'auth/wrong-password') {
+      alert('La contraseña es incorrecta');
+    } else if (errorCode == 'auth/user-not-found') {
+      alert('El usuario es incorrecto');
+    } else {
+      alert(errorMessage);
+    }
+  });
+});

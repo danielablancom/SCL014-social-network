a5e563caf46da1e4e28e724b59c54228056d7a06
Samantha <xsamynox@gmail.com>
2020-08-08 10:58:30 -0400

    modularizando y aplicando SPA, falta*

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
+import { login } from './lib/views/templateLogin.js';
+import { register } from './lib/views/templateRegister.js';
+import { changeRoute } from './lib/router.js';
 
-myFunction();
+// myFunction();
+// document.getElementById('root').innerHTML = login();
+// // document.getElementById('root').appenChild(login());
+
+
+const init = () => {
+  document.getElementById('root').innerHTML = login();
+  window.addEventListener('hashchange', () => {
+    document.getElementById('root').innerHTML = register();
+    myFunction();
+    changeRoute(window.location.hash);
+  });
+};
+
+window.addEventListener('load', init);
+
+
+/* const btnRegister = login().querySelector('#btn-register');
+btnRegister.addEventListener('click', () => {
+  changeRoute('#/register');
+}); */
